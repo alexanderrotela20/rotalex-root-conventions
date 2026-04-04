d0d38ab280b1e1ecde038c07a3479b4a1d1d1daf
Alexander Rotela <alexanderrotela2000@gmail.com>
2026-04-04 15:17:49 -0300

    refactor(convention): Configure Compose compiler metrics
    - Update `configureAndroidCompose.kt` to conditionally enable Compose compiler metrics and reports based on Gradle properties.
    - Add support for stability configuration files in Compose compiler settings.

--- convention/src/main/kotlin/io/github/alexanderrotela20/convention/ktx/configureAndroidCompose.kt
+++ convention/src/main/kotlin/io/github/alexanderrotela20/convention/ktx/configureAndroidCompose.kt
@@ -3,6 +3,7 @@ package io.github.alexanderrotela20.convention.ktx
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
@@ -25,7 +26,24 @@ internal fun Project.configureAndroidCompose(
     }
 
     extensions.configure<ComposeCompilerGradlePluginExtension> {
-        reportsDestination.set(layout.buildDirectory.dir("compose_compiler"))
-        metricsDestination.set(layout.buildDirectory.dir("compose_compiler"))
+
+        fun Provider<*>.relativeToRootProject(dir: String) = map {
+            @Suppress("UnstableApiUsage")
+            isolated.rootProject.projectDirectory
+                .dir("build")
+                .dir(projectDir.toRelativeString(rootDir))
+        }.map { it.dir(dir) }
+
+        project.providers.gradleProperty("enableComposeCompilerMetrics").flatMap { provider { it.takeIf(String::toBoolean)} }
+            .relativeToRootProject("compose-metrics")
+            .let(metricsDestination::set)
+
+        project.providers.gradleProperty("enableComposeCompilerReports").flatMap { provider { it.takeIf(String::toBoolean)} }
+            .relativeToRootProject("compose-reports")
+            .let(reportsDestination::set)
+
+        @Suppress("UnstableApiUsage")
+        stabilityConfigurationFiles
+            .add(isolated.rootProject.projectDirectory.file("compose_compiler_config.conf"))
     }
 }
